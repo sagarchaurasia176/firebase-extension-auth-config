087455e55b865ef28a244aed1782ff4bfa5fc90e
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2025-02-05 02:44:16 +0530

    auth design

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SignInWithPopup from './SignInWithPopup'
 import './App.css'
 const App = () => {
   return (
-    <div className=' bg-slate-800 '>
+    <div className=' bg-slate-200'>
       <SignInWithPopup/>
     </div>
   )
--- src/SignInWithPopup.tsx
+++ src/SignInWithPopup.tsx
@@ -5,12 +5,11 @@ const SignInWithPopup: React.FC = () => {
   return (
     <div className=" flex  justify-center  p-4 flex-col text-white items-center h-screen">
       <div className="text-center  p-2 text-2xl font-bold mb-4  transition duration-1000 ease-in-out">
-        <strong className=' p-4'>
-          <span className=" bg-white p-3 text-black">Welcome to TC-Analyzer:</span> 
-          <span className="bg-red-200 p-3 text-black"> Sign in to continue</span>
+        <strong className=' text-balance text-black  underline '>
+          Sign In to Your  Account
         </strong>
       </div>
-      <div className=' p-3'>
+      <div className=''>
       <button
         onClick={signIn}
         className="bg-black  px-12 py-3 rounded-xl  cursor-pointer  text-white font-bold py-2 px-4 rounded flex items-center  transition duration-300"
